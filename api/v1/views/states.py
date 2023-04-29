5af3003507ef46fe3a24cec1be5bfde1765da8f2
COLLINS  TONUI <tckiprotich@gmail.com>
2023-04-29 08:53:59 +0000

    view for State objects that handles all default RESTFul API actions:

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,3 +13,67 @@ def get_states():
     for state in storage.all(State).values():
         states.append(state.to_dict())
     return jsonify(states)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """ Retrieves a State object:
+    GET /api/v1/states/<state_id> """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """ Deletes a State object::
+    DELETE /api/v1/states/<state_id> """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """ Creates a State: POST /api/v1/states
+    If the HTTP body request is not valid JSON,
+    raise a 400 error with the message Not a JSON
+    If the dictionary doesn’t contain the key name,
+     raise a 400 error with the message Missing name
+ """
+    if not request.get_json():
+        abort(400, "Not a JSON")
+    if 'name' not in request.get_json():
+        abort(400, "Missing name")
+    state = State(**request.get_json())
+    storage.new(state)
+    storage.save()
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """ Updates a State object: PUT /api/v1/states/<state_id>
+    If the HTTP body request is not valid JSON,
+    raise a 400 error with the message Not a JSON
+    If the State id is not linked to any State object,
+    raise a 404 error
+    Otherwise, update the State object with all key-value
+    pairs of the dictionary
+    Ignore keys: id, created_at and updated_at
+    Return the State object: PUT /api/v1/states/<state_id> """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    if not request.get_json():
+        abort(400, "Not a JSON")
+    for key, value in request.get_json().items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    storage.save()
+    return jsonify(state.to_dict()), 200
