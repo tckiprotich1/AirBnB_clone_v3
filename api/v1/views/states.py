9469b403a5ac371d0c0019e4fe20e29009a71b75
COLLINS  TONUI <tckiprotich@gmail.com>
2023-04-29 08:56:03 +0000

    view for State objects that handles all default RESTFul API actions:

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,6 +4,8 @@ from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.state import State
+from datetime import datetime
+import uuid
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
