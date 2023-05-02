838cbea70b11d08a23ee3d9adc562cf7337870e6
COLLINS  TONUI <tckiprotich@gmail.com>
2023-05-02 15:18:53 +0000

    pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """ Module for __init.py__ """
 
-from flask import Flask, Blueprint, jsonify
-
-
-"""
-# create a variable app_views which is an instance of
- Blueprint (url prefix must be /api/v1)
-"""
+from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views.index import *
-# imports states
-from api.v1.views.states import *
+from api.v1.views.index import * 
+from api.v1.views.states import *  
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
+from api.v1.views.users import * 
+from api.v1.views.places import * 
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
