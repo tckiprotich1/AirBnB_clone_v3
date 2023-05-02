a350a170f705db55b92af2341f4f8cf99d378ac2
tckiprotich1 <132175061+tckiprotich1@users.noreply.github.com>
2023-05-02 21:30:07 +0000

    nn

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,3 +9,8 @@ from flask import Flask, Blueprint, jsonify
  Blueprint (url prefix must be /api/v1)
 """
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
+# imports states
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
